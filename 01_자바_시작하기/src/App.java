19adf1e920b5597e6b46478eb5ca7903ceba39af
HYEON <hyeon970315@gmail.com>
2024-09-09 14:33:38 +0900

    initialize JAVA class

---
+++ 01_자바_시작하기/src/App.java
@@ -0,0 +1,13 @@
+/**
+* "코딩 컨벤션"
+* @filename : App.java
+* @description : 자바 소스코드의 기본 구조를 파악하기 위한 예제
+* @author : 이승현 (hyeon970315@gmail.com)
+*/
+
+public class App {
+	public static void main( String[] args ) {
+		System.out.println("Hello World");
+		System.out.println("안녕하세요!");
+	}
+}
