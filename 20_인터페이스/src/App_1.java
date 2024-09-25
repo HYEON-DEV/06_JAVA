d6a71050064d5f7e98f577940e5d352a7018a328
HYEON <hyeon970315@gmail.com>
2024-09-25 20:59:43 +0900

    2024-09-25 study - interface, static, singleton pattern, exception, package, library

---
+++ 20_인터페이스/src/App_1.java
@@ -0,0 +1,25 @@
+public class App_1 {
+    public static void main(String[] args) {
+        
+        Monster m = new Monster("골램",500,30,200);
+        MainChar j = new MainChar("기사",1200,120,320);
+
+        m.walk();
+        m.run();
+        m.jump();
+
+        j.walk();
+        j.run();
+        j.jump();
+
+        m.attack();
+        j.defense();
+        j.attack();
+        m.evasion();
+        m.attack();
+        j.evasion();
+
+        j.pickUpItem();
+        j.talkNpc();
+    }
+}
